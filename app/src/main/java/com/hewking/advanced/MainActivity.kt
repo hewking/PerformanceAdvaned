087ff068b2a9e201bb32e92e7c56f49eee6df250
hewking <cjh1095358798@gmail.com>
2020-02-20 18:04:50 +0800

    update 卡顿优化测试

--- app/src/main/java/com/hewking/advanced/MainActivity.kt
+++ app/src/main/java/com/hewking/advanced/MainActivity.kt
@@ -2,11 +2,19 @@ package com.hewking.advanced
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_click.setOnClickListener {
+            // 模拟ANR 产生场景
+            Thread.sleep(30_1000L)
+            Log.d(MainActivity::class.java.simpleName,"after sleep")
+        }
     }
 }
