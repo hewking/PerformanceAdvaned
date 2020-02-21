c6c1b7457e277ff3378b21ee647fe23275857d34
hewking <cjh1095358798@gmail.com>
2020-02-21 23:39:37 +0800

    add CrashHandler

---
+++ app/src/main/java/com/hewking/util/ExecutorUtils.kt
@@ -0,0 +1,15 @@
+package com.hewking.util
+
+import java.util.concurrent.Executors
+
+/**
+ * @program: PerformanceAdvaned
+ * @author: hewking
+ * @create: 2020-02-21 23:22
+ * @description: 线程池工具类
+ **/
+object ExecutorUtils{
+
+    private val executors = Executors.newSingleThreadExecutor()
+
+}
