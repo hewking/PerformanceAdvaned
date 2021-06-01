07ea2e5c512ec4970081acc61c7584cb395c63ce
jianhao <chengjianhao@fcbox.com>
2021-06-01 11:42:42 +0800

    docs: add AsmPlugin

--- buildSrc/src/main/java/com/hewking/buildsrc/AsmPlugin.groovy
+++ buildSrc/src/main/java/com/hewking/buildsrc/AsmPlugin.groovy
@@ -1,18 +1,20 @@
-//package com.hewking.buildsrc;
-//
-//import org.gradle.api.Plugin;
-//import org.gradle.api.Project;
-//
-///**
-// * @author: jianhao
-// * @create: 2020/7/28
-// * @description:
-// */
-//class AsmPlugin implements Plugin<Project> {
-//    @Override
-//    void apply(Project project) {
-//        println("==================================")
-//        println("hello gradle plugin")
-//        println("==================================")
-//    }
-//}
+package com.hewking.buildsrc;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * @author: jianhao
+ * @create: 2020/7/28
+ * @description:
+ */
+class AsmPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        println("==================================")
+        println("hello gradle plugin")
+        println("==================================")
+    }
+
+}
